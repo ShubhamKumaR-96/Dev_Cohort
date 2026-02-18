88fc09d8d5e09d8c76041bdd1390d07e750ea29c
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2026-02-19 00:24:55 +0530

    updated component

--- React-State/src/App.jsx
+++ React-State/src/App.jsx
@@ -1,42 +1,12 @@
-import { createContext, useContext, useState } from "react";
 import "./App.css";
-
-const BulbContext = createContext();
+import Light from "./components/LightBlub";
 
 function App() {
-  const [bulbOn, setBulbOn] = useState(true);
-  return (
-    <BulbContext.Provider value={{ bulbOn, setBulbOn }}>
-      <LightBlub />
-    </BulbContext.Provider>
-  );
-}
-
-function LightBlub() {
-  // bulbOn is a prop of BulbState component
-  // bullOn and setBulbOn is a prop of ToggleBulbState component
-  return (
-    <div>
-      <BulbState />
-      <ToggleBulbState />
-    </div>
-  );
-}
-
-function BulbState() {
-  const { bulbOn } = useContext(BulbContext);
-  return <div>{bulbOn ? " bulb on" : "bulb off"}</div>;
-}
-
-function ToggleBulbState() {
-  const {bulbOn,setBulbOn}=useContext(BulbContext)
-  function toggleBulb() {
-    setBulbOn(!bulbOn);
-  }
+  
 
   return (
     <div>
-      <button onClick={toggleBulb}>Toggle the bulb</button>
+      <Light />
     </div>
   );
 }
---
+++ React-State/src/components/LightBlub.jsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useState } from "react";
+
+const BulbContext = createContext();
+
+function Light() {
+  const [bulbOn, setBulbOn] = useState(true);
+  return (
+    <BulbContext.Provider value={{ bulbOn, setBulbOn }}>
+      <LightBlub />
+    </BulbContext.Provider>
+  );
+}
+
+function LightBlub() {
+  // bulbOn is a prop of BulbState component
+  // bullOn and setBulbOn is a prop of ToggleBulbState component
+  return (
+    <div>
+      <BulbState />
+      <ToggleBulbState />
+    </div>
+  );
+}
+
+function BulbState() {
+  const { bulbOn } = useContext(BulbContext);
+  return <div>{bulbOn ? " bulb on" : "bulb off"}</div>;
+}
+
+function ToggleBulbState() {
+  const {bulbOn,setBulbOn}=useContext(BulbContext)
+  function toggleBulb() {
+    setBulbOn(!bulbOn);
+  }
+
+  return (
+    <div>
+      <button onClick={toggleBulb}>Toggle the bulb</button>
+    </div>
+  );
+}
+
+export default Light;
