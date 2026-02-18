a3ce97d9364b6f18bf7f19c6f6281a9ffc39183a
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2026-02-18 16:15:30 +0530

    case-1 state roll up

--- React-State/src/App.jsx
+++ React-State/src/App.jsx
@@ -1,13 +1,38 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-    Hi there
-    </>
-  )
+    <div>
+      <LightBlub />
+    </div>
+  );
 }
 
-export default App
+function LightBlub() {
+  const [bulbOn, setBlubOn] = useState(true);
+  return (
+    <div>
+      <BulbState bulbOn={bulbOn} />
+      <ToggleBulbState bulbOn={bulbOn} setBlubOn={setBlubOn} />
+    </div>
+  );
+}
+
+function BulbState({ bulbOn }) {
+  return <div>{bulbOn ? " bulb on" : "bulb off"}</div>;
+}
+
+function ToggleBulbState({ bulbOn, setBlubOn }) {
+  function toggleBulb() {
+    setBlubOn(!bulbOn);
+  }
+
+  return (
+    <div>
+      <button onClick={toggleBulb}>Toggle the bulb</button>
+    </div>
+  );
+}
+
+export default App;
